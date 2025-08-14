0202385513d86da656a44c03972a91ecebae65ff
Vedeneev Dmitry <basd97@yandex.ru>
2025-08-14 22:12:43 +0300

    states about history added

--- handlers/user_handler.py
+++ handlers/user_handler.py
@@ -1,11 +1,16 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 
 from keyboards.user_kb import user_kb
 
 user_router = Router()
+
+class Form(StatesGroup):
+    history = State()
+    name = State()
 
 @user_router.message(CommandStart())
 async def cmd_start(message: Message):
@@ -13,8 +18,22 @@ async def cmd_start(message: Message):
                          reply_markup= await user_kb())
 
 @user_router.message(F.text == "Рассказать историю")
-async def send_history(message: Message):
+async def send_history(message: Message, state: FSMContext):
+    await state.set_state(Form.history)
     await message.answer('Напиши свою историю!')
+
+@user_router.message(Form.history)
+async def get_history(message: Message, state: FSMContext):
+    await state.update_data(history=message.text)
+    await state.set_state(Form.name)
+    await message.answer('Спасибо за историю! Как тебя зовут?')
+
+
+@user_router.message(Form.name)
+async def get_name(message: Message, state: FSMContext):
+    data = await state.update_data(name=message.text)
+    await state.clear()
+    await message.answer('Спасибо! Информация принята')
 
 
 @user_router.message(F.text == 'Задать вопрос')
